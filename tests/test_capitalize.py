96a3cc15664d96d488d0addd9a1f72c457dd811e
zhdanovandrey4207 <zhdanovandrey4207@bk.ru>
2024-08-12 09:25:40 +0300

    Make first tests

---
+++ tests/test_capitalize.py
@@ -0,0 +1,13 @@
+from capitalize import capitalize
+
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно.')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно.')
+
+if capitalize(None) != None:
+    raise Exception('Функция работает неверно.')
+
+print('Все тесты пройдены')
