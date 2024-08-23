a825b5a0c1a4b6be774ec9f26db290cb1bc80ab9
zhdanovandrey4207 <zhdanovandrey4207@bk.ru>
2024-08-23 10:19:00 +0300

    Fix capitalize.py for type 'int'

--- package_name/capitalize.py
+++ package_name/capitalize.py
@@ -3,6 +3,8 @@ def capitalize(text):
         return ''
     if text == None:
         return None
+    if type(text) is int:
+        return text
     first_char = text[0].upper()
     rest_substring = text[1:]
     return f'{first_char}{rest_substring}'
